cd6348685ce95893b6065adc34ccc83eb9824082
pna16384 <pna16384@gmail.com>
2023-09-22 13:16:58 +0100

    Context versioning added and added function prototypes

--- glDemo/main.cpp
+++ glDemo/main.cpp
@@ -3,59 +3,92 @@
 #include <math.h>
 #include <iostream>
 
-/////////////////////////////////////////////////////////////////////////////////////
 // global variables
-/////////////////////////////////////////////////////////////////////////////////////
 
 // Window size
 unsigned int initWidth = 500;
 unsigned int initHeight = 500;
 
-
-/////////////////////////////////////////////////////////////////////////////////////
-// Function to call when window resized
-/////////////////////////////////////////////////////////////////////////////////////
-void resize(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);		// Draw into entire window
-}
+// Function prototypes
+void renderScene();
+void resizeWindow(GLFWwindow* window, int width, int height);
+void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
+void updateScene();
 
 
-/////////////////////////////////////////////////////////////////////////////////////
-// Function to call to handle keyboard input
-/////////////////////////////////////////////////////////////////////////////////////
-void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) 
-{
-	// ignore key up (key release) events
-	if (action == GLFW_RELEASE) 
-		return;	
+int main() {
 
-	// process keyboard inputs here..
-	switch (key)
+	//
+	// 1. Initialisation
+	//
+	
+
+	// Initialise glfw and setup window
+	glfwInit();
+
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+	glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE, GLFW_TRUE);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+
+	GLFWwindow* window = glfwCreateWindow(initWidth, initHeight, "GraphicsTemplate - Fixed Function Pipeline", NULL, NULL);
+	if (window == NULL)
 	{
-		case GLFW_KEY_ESCAPE:
-		case GLFW_KEY_X:
-			glfwSetWindowShouldClose(window, true);
-		break;
-
-		default:
-		{
-		}
+		fprintf(stdout, "Failed to create GLFW window!\n");
+		glfwTerminate();
+		return -1;
 	}
+	glfwMakeContextCurrent(window);
+	
+
+	// Set callback functions to handle different events
+	glfwSetFramebufferSizeCallback(window, resizeWindow); // resize window callback
+	glfwSetKeyCallback(window, keyboardHandler); // Keyboard input callback
+
+
+	// Initialise glew
+	glewInit();
+
+	
+	// Setup window's initial size
+	resizeWindow(window, initWidth, initHeight);
+
+	// Initialise scene - geometry and shaders etc
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // setup background colour to be black
+
+
+	//
+	// 2. Main loop
+	// 
+	
+
+	// Loop while program is not terminated.
+	while (!glfwWindowShouldClose(window)) {
+
+		updateScene();
+		renderScene();						// Render into the current buffer
+		glfwSwapBuffers(window);			// Displays what was just rendered (using double buffering).
+
+		// Poll events (key presses, mouse events)
+		// glfwWaitEvents();				// Use this if no animation.
+		// glfwWaitEventsTimeout(1.0/60.0);	// Use this to animate at 60 frames/sec (timing is NOT reliable)
+		glfwPollEvents();					// Use this version when animating as fast as possible
+	}
+
+	glfwTerminate();
+	return 0;
 }
 
 
-/////////////////////////////////////////////////////////////////////////////////////
-// renderScene
-/////////////////////////////////////////////////////////////////////////////////////
+
+// renderScene - function to render the current scene
 void renderScene()
 {
 	// Clear the rendering window
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	////////////////////////////////////////////////////////////////////////////////
-	// Render mesh objects here..
-	////////////////////////////////////////////////////////////////////////////////
+	// Render objects here...
 
 	glBegin(GL_TRIANGLES);
 
@@ -70,61 +103,38 @@ void renderScene()
 
 	glEnd();
 
-	glBindVertexArray(0);       	
+	glBindVertexArray(0);
 }
 
-/////////////////////////////////////////////////////////////////////////////////////
-// main()
-/////////////////////////////////////////////////////////////////////////////////////
-int main() 
+
+// Function to call when window resized
+void resizeWindow(GLFWwindow* window, int width, int height)
 {
-	// initialise glfw
-	glfwInit();
-
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
-
-	GLFWwindow* window = glfwCreateWindow(initWidth, initHeight, "GraphicsTemplate - Fixed Function Pipeline", NULL, NULL);
-	if (window == NULL)
-	{
-		fprintf(stdout, "Failed to create GLFW window!\n");
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	
-	
-	// initialise glew
-	glewInit();
-
-
-	// Set callback function for resizing the window
-	glfwSetFramebufferSizeCallback(window, resize);
-
-	// Set callback for keyboard events
-	glfwSetKeyCallback(window, keyboard);
-
-
-	resize(window, initWidth, initHeight);
-
-	////////////////////////////////////////////////////
-	// initialise scene - geometry and shaders etc
-	////////////////////////////////////////////////////
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-
-
-	// Loop while program is not terminated.
-	while (!glfwWindowShouldClose(window)) {
-
-		renderScene();						// Render into the current buffer
-		glfwSwapBuffers(window);			// Displays what was just rendered (using double buffering).
-
-		// Poll events (key presses, mouse events)
-		// glfwWaitEvents();				// Use this if no animation.
-		// glfwWaitEventsTimeout(1.0/60.0);	// Use this to animate at 60 frames/sec (timing is NOT reliable)
-		glfwPollEvents();					// Use this version when animating as fast as possible
-	}
-
-	glfwTerminate();
-	return 0;
+	glViewport(0, 0, width, height);		// Draw into entire window
 }
+
+// Function to call to handle keyboard input
+void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// ignore key up (key release) events
+	if (action == GLFW_RELEASE)
+		return;
+
+	// process keyboard inputs here..
+	switch (key)
+	{
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(window, true);
+			break;
+
+		default:
+		{
+		}
+	}
+}
+
+
+// Function called to animate elements in the scene
+void updateScene() {
+}
+
